020fd62adbdd94025268d25f6ab01b8e39f2d763
Shira Salomon <shy200504@gmail.com>
2023-10-22 23:33:13 +0300

    change name

---
+++ Stage0/Program5046.cs
@@ -0,0 +1,23 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome5046();
+        //Console.Readkey();
+        Welcome8812();
+    }
+
+    private static void Welcome5046()
+    {
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application ", userName);
+    }
+
+    private static void Welcome8812()
+    {
+
+    }
+
+}
+
