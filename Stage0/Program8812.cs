dbcba4936816692a211a11ea6fde70f8b545cf17
טליה כהן <taliachoen1@gmail.com>
2023-10-25 21:59:51 +0300

    more!!

--- Stage0/Program8812.cs
+++ Stage0/Program8812.cs
@@ -10,7 +10,7 @@ namespace Stage0
     {
         static partial void Welcome8812()
         {
-            Console.WriteLine("I am also here!");
+            Console.WriteLine("I am also here!!!!");
         }
     }
 }
