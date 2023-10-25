039a2502b2032dc9f6959590e9049eddf8097a00
טליה כהן <taliachoen1@gmail.com>
2023-10-25 21:56:29 +0300

    newAct+Welcome

--- Stage0/Program5046.cs
+++ Stage0/Program5046.cs
@@ -1,23 +1,31 @@
-﻿partial class Program
-{
-    private static void Main(string[] args)
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0 {
+
+    partial class Program
     {
-        Welcome5046();
-        //Console.Readkey();
-        Welcome8812();
+        static void Main(string[] args)
+        {
+            Welcome5046();
+            //Console.Readkey();
+            Welcome8812();
+        }
+
+        static partial void Welcome8812();
+
+        private static void Welcome5046()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application ", userName);
+        }
+
     }
-
-    private static void Welcome5046()
-    {
-        Console.Write("Enter your name: ");
-        string userName = Console.ReadLine();
-        Console.WriteLine("{0}, welcome to my first console application ", userName);
-    }
-
-    private static void Welcome8812()
-    {
-
-    }
-
 }
+
 
